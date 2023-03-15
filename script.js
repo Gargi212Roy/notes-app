d166dbbddc8102e8e4685b0aedf7b4ccc418eb88
itsgargiroy <itsgargiroy@gmail.com>
2023-03-15 17:56:08 +0530

    localStorage added

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const addBtn = document.getElementById("add");
 
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+}
+
 addBtn.addEventListener("click", () => addNewNote());
 
 function addNewNote(text = "Hi, how's your day going?") {
@@ -25,6 +31,7 @@ function addNewNote(text = "Hi, how's your day going?") {
 
   deleteBtn.addEventListener("click", () => {
     note.remove();
+    updateLS();
   });
   editBtn.addEventListener("click", () => {
     main.classList.toggle("hidden");
@@ -33,7 +40,18 @@ function addNewNote(text = "Hi, how's your day going?") {
   textArea.addEventListener("input", (e) => {
     const { value } = e.target;
     main.innerHTML = value;
+
+    updateLS();
   });
 
   document.body.appendChild(note);
+}
+
+function updateLS() {
+  const notesText = document.querySelectorAll("textarea");
+
+  const notes = [];
+  notesText.forEach((note) => notes.push(note.value));
+
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
